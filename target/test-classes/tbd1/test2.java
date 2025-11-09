55bfd0b3ec3b5693da0871d95510786291170de1
aggelosalexo <aggelostelemaxos@gmail.com>
2025-11-09 16:51:36 +0200

    Update test2.java

--- target/test-classes/tbd1/test2.java
+++ target/test-classes/tbd1/test2.java
@@ -2,9 +2,20 @@ public class test2 {
     int counter;
     String name;
     Double sul;
-    public String returnname (int name){
+    public test2(String name,Double sul){
+        this.name=name;
+        this.sul=sul;
+        this.counter=0;
+    }
+    public String returnname (String name){
         counter ++;
         return name;
 
+    }
+    public int getCounter(){
+        return counter;
+    }
+    public Double getSul(){
+        return sul;
     }
 }
