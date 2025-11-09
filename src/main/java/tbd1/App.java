5403f9edfd5151e1f29a86dcf032120e40bb6a53
aggelosalexo <aggelostelemaxos@gmail.com>
2025-11-09 15:51:00 +0200

    Update App.java

--- src/main/java/tbd1/App.java
+++ src/main/java/tbd1/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int x=0;
+        if (x=0){
+            System.out.println("ακ");
+        }
     }
 }
