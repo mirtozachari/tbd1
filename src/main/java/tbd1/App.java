adcd4282df7ec91e3aef80a8334b03b649a790f7
aggelosalexo <aggelostelemaxos@gmail.com>
2025-11-11 16:18:57 +0200

    Update App.java

--- src/main/java/tbd1/App.java
+++ src/main/java/tbd1/App.java
@@ -1,16 +1,9 @@
 package tbd1;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-      test2 t = new test2("Γιαννης",165.2);
-      System.out.println(t.returnName());
-
+    public static void main( String[] args ){
 
     }
+    
 }
