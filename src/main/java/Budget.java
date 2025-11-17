c6cab7fb4ccb643485603d90e178ad4d5fab0f28
tz3ni <t8240140@aueb.gr>
2025-11-17 14:46:54 +0200

    Create Budget.java

---
+++ src/main/java/Budget.java
@@ -0,0 +1,28 @@
+import com.google.gson.Gson;
+import java.io.FileReader;
+import java.io.IOException;
+public class Budget {
+    private String category;
+    private int amount;
+    public String getCategory() {
+        return category;
+    }
+    public int getAmount() {
+        return amount;
+    }
+    public void setAmount(int amount) {
+        this.amount = amount; 
+    }
+    public static void main(String[] args) {
+    Gson gson = new Gson();
+    try (FileReader reader = new FileReader("GreakBudget25.json")) {
+        Budget[] budgets = gson.fromJson(reader, Budget[].class);
+    for (Budget b : budgets) {
+        System.out.println("κατηγορία:" + b.getCategory());
+        System.out.println("ποσό:" + b.getAmount());
+    } 
+} catch (IOException e) {
+        e.printStackTrace();
+    }
+  }
+}
