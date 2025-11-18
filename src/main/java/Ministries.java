ae3137db8007f5048bd5cfda1133db88d2ab3c32
tz3ni <t8240140@aueb.gr>
2025-11-18 15:14:16 +0200

    Create Ministries.java

---
+++ src/main/java/Ministries.java
@@ -0,0 +1,30 @@
+import com.google.gson.Gson;
+import java.io.FileReader;
+import java.io.IOException;
+public class Ministries {
+    private String category;
+    private int amount;
+    public String getCategory() {
+        return category;
+    }
+    public int getAmount() {
+        return amount;
+    }
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        try(FileReader reader = new FileReader("src/main/resources/config/ministries25.json")) {
+            Ministries[] ministry = gson.fromJson(reader, Ministries[].class);
+            int totall = 0;
+            for (Ministries m : ministry) {
+                System.out.println("υπουργείο:" + m.getCategory());
+                System.out.println("ποσό:" + m.getAmount());
+                totall += m.getAmount();
+            }
+        } catch (IOException e) {
+        e.printStackTrace();
+    }
+    }
+}
