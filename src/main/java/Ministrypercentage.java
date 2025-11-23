293b2e7334a7bf2cbc42dcc7f4ad776b3e8f30b7
tz3ni <t8240140@aueb.gr>
2025-11-23 19:53:47 +0200

    Create Ministrypercentage.java

---
+++ src/main/java/Ministrypercentage.java
@@ -0,0 +1,18 @@
+public class Ministrypercentage implements Displayable {
+    private Ministries[] ministries;
+    public Ministrypercentage(Ministries[] ministries) {
+        this.ministries = ministries;
+    }
+    @Override
+    public void display() {
+        long total = 0;
+        double per;
+        for (Ministries m : ministries){
+        total += m.getAmount();
+        }
+        for (Ministries m : ministries) {
+            per = 100.0 * m.getAmount() / total;
+            System.out.println("Το Υπουργείο:" + m.getCategory() + " καλύπτει " + String.format("%.4f",per) + "%" + " επί του συνολικού προϋπολογισμού");
+        }
+    }
+}
