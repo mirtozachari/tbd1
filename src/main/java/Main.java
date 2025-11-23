8e5ff6b94caebb15aec320d420903e67a5d04848
aggelosalexo <aggelostelemaxos@gmail.com>
2025-11-23 17:39:54 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
-public class Main
-{
-    public static void main( String[] args ){
-        Ministries.getMinistry();
-        Revenues.getRevenues();
+import com.google.gson.Gson;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) {
+        Ministries[] ministries = new Ministries[0];
+        try (FileReader reader = new FileReader("src/main/resources/config/ministries25.json")) {
+            ministries = new Gson().fromJson(reader, Ministries[].class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        MinistryPrinter printer = new MinistryPrinter(ministries);
+        printer.display();
     }
-
 }
+    
