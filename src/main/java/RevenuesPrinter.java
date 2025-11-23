f0f1cf15c883ec762563c38c5bb03b5217202fd5
aggelosalexo <aggelostelemaxos@gmail.com>
2025-11-23 23:11:43 +0200

    Create RevenuesPrinter.java

---
+++ src/main/java/RevenuesPrinter.java
@@ -0,0 +1,20 @@
+public class RevenuesPrinter implements Displayable {
+     private Revenues[] revenues;
+
+    public RevenuesPrinter(Revenues[] revenues) {
+        this.revenues = revenues;
+    }
+
+    @Override
+    public void display() {
+        long total = 0;
+
+        for (Revenues r : revenues) {
+            System.out.println("Κατηγορία: " + r.getCategory());
+            System.out.println("Ποσό: " + r.getAmount());
+            total += r.getAmount();
+        }
+
+        System.out.println("Συνολικό Ποσό: " + total);
+    }
+}
