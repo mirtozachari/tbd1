b2c95dcf9512114dd5135b7322b66b3415374477
aggelosalexo <aggelostelemaxos@gmail.com>
2025-11-23 23:11:48 +0200

    Create RevenuesLoader.java

---
+++ src/main/java/RevenuesLoader.java
@@ -0,0 +1,15 @@
+import com.google.gson.Gson;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class RevenuesLoader {
+    public static Revenues[] load(String path) {
+        Gson gson = new Gson();
+        try (FileReader reader = new FileReader(path)) {
+            return gson.fromJson(reader, Revenues[].class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new Revenues[0];
+        }
+    }
+}
