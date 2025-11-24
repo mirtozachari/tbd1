2f41b29cd8420844e28e6612d25cebcbb591aca4
aggelosalexo <aggelostelemaxos@gmail.com>
2025-11-24 20:16:07 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,14 @@ public class Main {
     public static void main(String[] args) {
         Ministries[] ministries = MinistryLoader.load("src/main/resources/config/ministries25.json");
         Revenues[] revenues = RevenuesLoader.load("src/main/resources/config/revenue25.json");
+        Expenses[] expenses = ExpensesLoader.load("src/main/resources/config/expenses25.json");
         MinistryPrinter ministryPrinter = new MinistryPrinter(ministries);
         ministryPrinter.display();
         Ministrypercentage ministryPercentage = new Ministrypercentage(ministries);
         ministryPercentage.display();
         RevenuesPrinter revenuesPrinter = new RevenuesPrinter(revenues);
         revenuesPrinter.display();
+        ExpensesPrinter expensesPrinter = new ExpensesPrinter(expenses);
+        expensesPrinter.display();
     }
 }
