2b668c86ad5a2e8df33561d57565a3381e2f596b
tz3ni <t8240140@aueb.gr>
2025-11-24 20:00:17 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Δώσε αριθμό επιλογής για το μενού");
+        int x = scanner.nextInt();
         Ministries[] ministries = MinistryLoader.load("src/main/resources/config/ministries25.json");
         Revenues[] revenues = RevenuesLoader.load("src/main/resources/config/revenue25.json");
+        switch (x) {
+            case 0:
         MinistryPrinter ministryPrinter = new MinistryPrinter(ministries);
         ministryPrinter.display();
+        break;
+            case 1:
         Ministrypercentage ministryPercentage = new Ministrypercentage(ministries);
         ministryPercentage.display();
+        break;
+            case 2:
         RevenuesPrinter revenuesPrinter = new RevenuesPrinter(revenues);
         revenuesPrinter.display();
+        break;
+        }
+
     }
 }
