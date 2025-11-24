9c74996116298559134f15769b9bce038926ae5d
tz3ni <t8240140@aueb.gr>
2025-11-24 20:33:44 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,9 @@ public class Main {
         int x = scanner.nextInt();
         Ministries[] ministries = MinistryLoader.load("src/main/resources/config/ministries25.json");
         Revenues[] revenues = RevenuesLoader.load("src/main/resources/config/revenue25.json");
-<<<<<<< HEAD
         Expenses[] expenses = ExpensesLoader.load("src/main/resources/config/expenses25.json");
-=======
         switch (x) {
             case 0:
->>>>>>> 73b31480227b27f94b8a29797063ac25e41f339e
         MinistryPrinter ministryPrinter = new MinistryPrinter(ministries);
         ministryPrinter.display();
         break;
@@ -22,15 +19,17 @@ public class Main {
             case 2:
         RevenuesPrinter revenuesPrinter = new RevenuesPrinter(revenues);
         revenuesPrinter.display();
-<<<<<<< HEAD
-        ExpensesPrinter expensesPrinter = new ExpensesPrinter(expenses);
-        expensesPrinter.display();
-=======
         break;
             case 3:
+        ExpensesPrinter expensesPrinter = new ExpensesPrinter(expenses);
+        expensesPrinter.display();
+        break;
+            case 4
+            :
         System.out.println("Δες τα στοιχεία του Υπουργείου που σε ενδιαφέρει:");
         break;
+        default:
+            System.out.println("Άκυρος αριθμός");
         }
->>>>>>> 73b31480227b27f94b8a29797063ac25e41f339e
     }
 }
