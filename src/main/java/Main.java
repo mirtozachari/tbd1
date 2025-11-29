5cdfea6dc141540708c5067ff5159940f10d6050
aggelosalexo <aggelostelemaxos@gmail.com>
2025-11-29 13:17:16 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,34 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);// ξεκινάει το μενού επιλογών του χρήστη
         System.out.println("Δώσε αριθμό επιλογής για το μενού");
         int x = scanner.nextInt();
         Ministries[] ministries = MinistryLoader.load("src/main/resources/config/ministries25.json");
         Revenues[] revenues = RevenuesLoader.load("src/main/resources/config/revenue25.json");
         Expenses[] expenses = ExpensesLoader.load("src/main/resources/config/expenses25.json");
         switch (x) {
-            case 0:
+            case 0: // εμφάνιση στοιχείων όλων των υπουργείων
         MinistryPrinter ministryPrinter = new MinistryPrinter(ministries);
         ministryPrinter.display();
         break;
-            case 1:
+            case 1://εμφάνιση ποσοστών κάθε υπουργείου επί του συνολικού προϋπολογισμού
         Ministrypercentage ministryPercentage = new Ministrypercentage(ministries);
         ministryPercentage.display();
         break;
-            case 2:
+            case 2://εμφάνιση όλων των εσόδων 
         RevenuesPrinter revenuesPrinter = new RevenuesPrinter(revenues);
         revenuesPrinter.display();
         break;
-            case 3:
+            case 3://εμφάνιση όλων των εξόδων
         ExpensesPrinter expensesPrinter = new ExpensesPrinter(expenses);
         expensesPrinter.display();
         break;
-            case 4
+            case 4//εμφάνιση προϋπολογισμού συγκεκριμένου υπουργείου που επιλέγει ο χρήστης
             :
         System.out.println("Δες τα στοιχεία του Υπουργείου που σε ενδιαφέρει:");
+        MenuForMinistries menu = new MenuForMinistries(ministries);
+        menu.Menu();
         break;
         default:
             System.out.println("Άκυρος αριθμός");
