3e7cf072c24f39890d4f2c187b129dca13f91157
aggelosalexo <aggelostelemaxos@gmail.com>
2025-11-29 13:17:11 +0200

    Update MenuForMinistries.java

--- src/main/java/MenuForMinistries.java
+++ src/main/java/MenuForMinistries.java
@@ -1,28 +1,39 @@
 import java.util.Scanner;
 public class MenuForMinistries {
    private Ministries[] ministries;
-    public MinistryPrinter(Ministries[] ministries) {
+    public MenuForMinistries(Ministries[] ministries) {
         this.ministries = ministries;
-        }
-
     }
-    @Override
-    public void display() {
-        boolean flag;
-        while (flag =false) {   
+    public void Menu() {
+        System.out.println("Δώσε υπουργείο:");
+        boolean flag = false;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Δώσε υπουργείο");
-        String x = scanner.nextString();
-        for (Ministries m : ministries) {
-             if (x = m.getCategory()) {
-            System.out.println(m.getAmount);
-            flag = true;
-            break;
+        while (flag == false) {
+       System.out.println("Διαθέσιμα Υπουργεία και οι αριθμοί τους:");
+       for (int i = 0; i < ministries.length; i++) {
+        System.out.println(i+1 + ". " + ministries[i].getCategory());
+       }
+        System.out.println(ministries.length + 1 + ". " +  "Έξοδος");
+        System.out.println("Δώσε τον αριθμό του Υπουργείου που σε ενδιαφέρει:");
+        String x = scanner.nextLine().trim(); 
+        // βάζουμε nextLine() αντί για nextInt() για να μην "σκάσει" το πρόγραμμα σε περίπτωση εισόδου μιας σειρά χαρακτήρων αντί για τον ζητούμενο αριθμό
+        try {
+            int y = Integer.parseInt(x);
+            //μετατρέπει τον αριθμό από τύπου String σε τύπου int
+            if (y >= 1 && y < ministries.length ) {
+                Ministries m = ministries[y - 1];
+                System.out.print("Επέλεξες το: " +  m.getCategory() + " ");
+                System.out.println("με ποσό: " + m.getAmount() + "ευρώ");
+            } else if (y == ministries.length + 1) {
+                System.out.println("Έξοδος από το μενού");
+                flag = true;
+            } else {
+                System.out.println("Δεν βρέθηκε το Υπουργείο. Δοκιμάστε ξανά.");
             }
+        } catch (NumberFormatException e) {
+            System.out.println("Πρέπει να δώσεις αριθμό. Ξαναδοκίμασε.");
         }
-        if (flag = false) {
-            System.out.println("Δεν υπάρχει το υπουργείο");
-            System.out.println("Ξαναδώσε όνομα υπουργείου");
-        }
+        }    
+    }     
     }
-}
+
