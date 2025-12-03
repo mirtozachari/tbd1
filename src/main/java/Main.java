150011ad3f205f71d94c02e07c5a5f8f759703d8
Aristeidis Anagnostopoulos <t8230241@aueb.gr>
2025-12-03 08:53:56 +0200

    Add while loop in Μain until user enters 100

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,42 +5,57 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in); // Ξεκινάει το μενού επιλογών του χρήστη
-        System.out.println("Δώσε αριθμό επιλογής για το μενού");
-        int x = scanner.nextInt();
+        
+        int x = 0;
 
-        Ministries[] ministries = MinistryLoader.load("src/main/resources/config/ministries25.json");
-        Revenues[] revenues = RevenuesLoader.load("src/main/resources/config/revenue25.json");
-        Expenses[] expenses = ExpensesLoader.load("src/main/resources/config/expenses25.json");
+        while (x != 100) {     
+            System.out.println("Δώσε αριθμό επιλογής για το μενού");
+            System.out.println("Για εμφάνιση στοιχείων όλων των υπουργείων πάτα 0");
+            System.out.println("Για εμφάνιση ποσοστών κάθε υπουργείου επί του συνολικού προϋπολογισμού πάτα 1");
+            System.out.println("Για εμφάνιση όλων των εσόδων πάτα 2");
+            System.out.println("Για εμφάνιση όλων των εξόδων πάτα 3");
+            System.out.println("Για εμφάνιση στοιχείων όλων των υπουργείων και επιλογή ενός πάτα 4");
+            System.out.println("Για έξοδο από την εφαρμογή πάτα 100");
+            x = scanner.nextInt();
 
-        switch (x) {
-            case 0: // Εμφάνιση στοιχείων όλων των υπουργείων
-                MinistryPrinter ministryPrinter = new MinistryPrinter(ministries);
-                ministryPrinter.display();
-                break;
+            Ministries[] ministries = MinistryLoader.load("src/main/resources/config/ministries25.json");
+            Revenues[] revenues = RevenuesLoader.load("src/main/resources/config/revenue25.json");
+            Expenses[] expenses = ExpensesLoader.load("src/main/resources/config/expenses25.json");
 
-            case 1: // Εμφάνιση ποσοστών κάθε υπουργείου επί του συνολικού προϋπολογισμού
-                Ministrypercentage ministryPercentage = new Ministrypercentage(ministries);
-                ministryPercentage.display();
-                break;
+            switch (x) {
+                case 0: // Εμφάνιση στοιχείων όλων των υπουργείων
+                    MinistryPrinter ministryPrinter = new MinistryPrinter(ministries);
+                    ministryPrinter.display();
+                    break;
 
-            case 2: // Εμφάνιση όλων των εσόδων 
-                RevenuesPrinter revenuesPrinter = new RevenuesPrinter(revenues);
-                revenuesPrinter.display();
-                break;
+                case 1: // Εμφάνιση ποσοστών κάθε υπουργείου επί του συνολικού προϋπολογισμού
+                    Ministrypercentage ministryPercentage = new Ministrypercentage(ministries);
+                    ministryPercentage.display();
+                    break;
 
-            case 3: // Εμφάνιση όλων των εξόδων
-                ExpensesPrinter expensesPrinter = new ExpensesPrinter(expenses);
-                expensesPrinter.display();
-                break;
+                case 2: // Εμφάνιση όλων των εσόδων 
+                    RevenuesPrinter revenuesPrinter = new RevenuesPrinter(revenues);
+                    revenuesPrinter.display();
+                    break;
 
-            case 4: // Εμφάνιση προϋπολογισμού συγκεκριμένου υπουργείου που επιλέγει ο χρήστης
-                System.out.println("Δες τα στοιχεία του Υπουργείου που σε ενδιαφέρει:");
-                MenuForMinistries menu = new MenuForMinistries(ministries);
-                menu.Menu();
-                break;
+                case 3: // Εμφάνιση όλων των εξόδων
+                    ExpensesPrinter expensesPrinter = new ExpensesPrinter(expenses);
+                    expensesPrinter.display();
+                    break;
 
-            default:
-                System.out.println("Άκυρος αριθμός");
+                case 4: // Εμφάνιση προϋπολογισμού συγκεκριμένου υπουργείου που επιλέγει ο χρήστης
+                    System.out.println("Δες τα στοιχεία του Υπουργείου που σε ενδιαφέρει:");
+                    MenuForMinistries menu = new MenuForMinistries(ministries);
+                    menu.Menu();
+                    break;
+
+                case 100: // Έξοδος από την εφαρμογή
+                    System.out.println("Καληνύχτα κύριε πρωθυπουργέ!");
+                    break;
+                
+                default:
+                    System.out.println("Άκυρος αριθμός");
+            }
         }
     }
 }
