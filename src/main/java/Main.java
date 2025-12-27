b7869e9b6e56ceedf529f466b5a036c429b9d50d
Aristeidis Anagnostopoulos <t8230241@aueb.gr>
2025-12-27 13:40:30 +0200

    Created year selection menu in MAIN.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,70 +7,20 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in); // Ξεκινάει το μενού επιλογών του χρήστη
         
-        int x = 0;
+        
+        System.out.println("ΕΠΕΛΕΞΕ ΕΤΟΣ:2023,2024,2025");
+        int yearSelection = scanner.nextInt();
+        if (yearSelection == 2023) {
+            MainMenu.Menu2023();
 
-        while (x != 100) {     
-            System.out.println("Δώσε αριθμό επιλογής για το μενού");
-            System.out.println("Για εμφάνιση στοιχείων όλων των υπουργείων πάτα 0");
-            System.out.println("Για εμφάνιση ποσοστών κάθε υπουργείου επί του συνολικού προϋπολογισμού πάτα 1");
-            System.out.println("Για εμφάνιση όλων των εσόδων πάτα 2");
-            System.out.println("Για εμφάνιση όλων των εξόδων πάτα 3");
-            System.out.println("Για εμφάνιση στοιχείων όλων των υπουργείων και επιλογή ενός πάτα 4");
-            System.out.println("Για εισαγωγή αλλαγών πάτα 5");
-            System.out.println("Για να εμφανιστούν γραφήματα πάτα 6:");
-            System.out.println("Για έξοδο από την εφαρμογή πάτα 100");
-            x = scanner.nextInt();
-
-            Ministries[] ministries = MinistryLoader.load("src/main/resources/config/ministries25.json");
-            Revenues[] revenues = RevenuesLoader.load("src/main/resources/config/revenues25.json");
-            Expenses[] expenses = ExpensesLoader.load("src/main/resources/config/expenses25.json");
-
-            switch (x) {
-                case 0: // Εμφάνιση στοιχείων όλων των υπουργείων
-                    MinistryPrinter ministryPrinter = new MinistryPrinter(ministries);
-                    ministryPrinter.display();
-                    break;
-
-                case 1: // Εμφάνιση ποσοστών κάθε υπουργείου επί του συνολικού προϋπολογισμού
-                    Ministrypercentage ministryPercentage = new Ministrypercentage(ministries);
-                    ministryPercentage.display();
-                    break;
-
-                case 2: // Εμφάνιση όλων των εσόδων 
-                    RevenuesPrinter revenuesPrinter = new RevenuesPrinter(revenues);
-                    revenuesPrinter.display();
-                    break;
-
-                case 3: // Εμφάνιση όλων των εξόδων
-                    ExpensesPrinter expensesPrinter = new ExpensesPrinter(expenses);
-                    expensesPrinter.display();
-                    break;
-
-                case 4: // Εμφάνιση προϋπολογισμού συγκεκριμένου υπουργείου που επιλέγει ο χρήστης
-                    System.out.println("Δες τα στοιχεία του Υπουργείου που σε ενδιαφέρει:");
-                    MenuForMinistries menu = new MenuForMinistries(ministries);
-                    menu.Menu();
-                    break;
-                case 5: // Δυνατότητα εισαγωγής αλλαγών
-                    System.out.println("Δώσε τις επιθυμητές αλλαγές:");
-                    break;
-                case 6: // Γραφήματα
-                System.out.println("Δες τα γραφήματα:");
-                 try {
-            diagramministries.diagram();  
-                 } catch (Exception e) {
-            e.printStackTrace();
-            }
-                break;
-
-                case 100: // Έξοδος από την εφαρμογή
-                    System.out.println("Καληνύχτα κύριε πρωθυπουργέ!");
-                    break;
+        } else if (yearSelection == 2024)  {
+            MainMenu.Menu2024();
                 
-                default:
-                    System.out.println("Άκυρος αριθμός");
-            }
+        } else if (yearSelection == 2025) {
+            MainMenu.Menu2025();
+            
         }
+
         scanner.close();
     }
 }
---
+++ src/main/java/MainMenu.java
@@ -0,0 +1,215 @@
+import java.util.Scanner;
+
+public class MainMenu {
+
+
+    public static void Menu2023() {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = 0;        
+        while (x != 100) {     
+            System.out.println("Δώσε αριθμό επιλογής για το μενού");
+            System.out.println("Για εμφάνιση στοιχείων όλων των υπουργείων πάτα 0");
+            System.out.println("Για εμφάνιση ποσοστών κάθε υπουργείου επί του συνολικού προϋπολογισμού πάτα 1");
+            System.out.println("Για εμφάνιση όλων των εσόδων πάτα 2");
+            System.out.println("Για εμφάνιση όλων των εξόδων πάτα 3");
+            System.out.println("Για εμφάνιση στοιχείων όλων των υπουργείων και επιλογή ενός πάτα 4");
+            System.out.println("Για εισαγωγή αλλαγών πάτα 5");
+            System.out.println("Για να εμφανιστούν γραφήματα πάτα 6:");
+            System.out.println("Για έξοδο από την εφαρμογή πάτα 100");
+            x = scanner.nextInt();
+
+            Ministries[] ministries = MinistryLoader.load("src/main/resources/config/ministries23.json");
+            Revenues[] revenues = RevenuesLoader.load("src/main/resources/config/revenues23.json");
+            Expenses[] expenses = ExpensesLoader.load("src/main/resources/config/expenses23.json");
+
+            switch (x) {
+                case 0: // Εμφάνιση στοιχείων όλων των υπουργείων
+                    MinistryPrinter ministryPrinter = new MinistryPrinter(ministries);
+                    ministryPrinter.display();
+                    break;
+
+                case 1: // Εμφάνιση ποσοστών κάθε υπουργείου επί του συνολικού προϋπολογισμού
+                    Ministrypercentage ministryPercentage = new Ministrypercentage(ministries);
+                    ministryPercentage.display();
+                    break;
+
+                case 2: // Εμφάνιση όλων των εσόδων 
+                    RevenuesPrinter revenuesPrinter = new RevenuesPrinter(revenues);
+                    revenuesPrinter.display();
+                    break;
+
+                case 3: // Εμφάνιση όλων των εξόδων
+                    ExpensesPrinter expensesPrinter = new ExpensesPrinter(expenses);
+                    expensesPrinter.display();
+                    break;
+
+                case 4: // Εμφάνιση προϋπολογισμού συγκεκριμένου υπουργείου που επιλέγει ο χρήστης
+                    System.out.println("Δες τα στοιχεία του Υπουργείου που σε ενδιαφέρει:");
+                    MenuForMinistries menu = new MenuForMinistries(ministries);
+                    menu.Menu();
+                    break;
+                case 5: // Δυνατότητα εισαγωγής αλλαγών
+                    System.out.println("Δώσε τις επιθυμητές αλλαγές:");
+                    break;
+                case 6: // Γραφήματα
+                System.out.println("Δες τα γραφήματα:");
+                 try {
+            diagramministries.diagram();  
+                 } catch (Exception e) {
+            e.printStackTrace();
+            }
+                break;
+
+                case 100: // Έξοδος από την εφαρμογή
+                    System.out.println("Καληνύχτα κύριε πρωθυπουργέ!");
+                    break;
+                
+                default:
+                    System.out.println("Άκυρος αριθμός");
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static void Menu2024() {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = 0;
+        while (x != 100) {     
+            System.out.println("Δώσε αριθμό επιλογής για το μενού");
+            System.out.println("Για εμφάνιση στοιχείων όλων των υπουργείων πάτα 0");
+            System.out.println("Για εμφάνιση ποσοστών κάθε υπουργείου επί του συνολικού προϋπολογισμού πάτα 1");
+            System.out.println("Για εμφάνιση όλων των εσόδων πάτα 2");
+            System.out.println("Για εμφάνιση όλων των εξόδων πάτα 3");
+            System.out.println("Για εμφάνιση στοιχείων όλων των υπουργείων και επιλογή ενός πάτα 4");
+            System.out.println("Για εισαγωγή αλλαγών πάτα 5");
+            System.out.println("Για να εμφανιστούν γραφήματα πάτα 6:");
+            System.out.println("Για έξοδο από την εφαρμογή πάτα 100");
+            x = scanner.nextInt();
+
+            Ministries[] ministries = MinistryLoader.load("src/main/resources/config/ministries24.json");
+            Revenues[] revenues = RevenuesLoader.load("src/main/resources/config/revenues24.json");
+            Expenses[] expenses = ExpensesLoader.load("src/main/resources/config/expenses24.json");
+
+            switch (x) {
+                case 0: // Εμφάνιση στοιχείων όλων των υπουργείων
+                    MinistryPrinter ministryPrinter = new MinistryPrinter(ministries);
+                    ministryPrinter.display();
+                    break;
+
+                case 1: // Εμφάνιση ποσοστών κάθε υπουργείου επί του συνολικού προϋπολογισμού
+                    Ministrypercentage ministryPercentage = new Ministrypercentage(ministries);
+                    ministryPercentage.display();
+                    break;
+
+                case 2: // Εμφάνιση όλων των εσόδων 
+                    RevenuesPrinter revenuesPrinter = new RevenuesPrinter(revenues);
+                    revenuesPrinter.display();
+                    break;
+
+                case 3: // Εμφάνιση όλων των εξόδων
+                    ExpensesPrinter expensesPrinter = new ExpensesPrinter(expenses);
+                    expensesPrinter.display();
+                    break;
+
+                case 4: // Εμφάνιση προϋπολογισμού συγκεκριμένου υπουργείου που επιλέγει ο χρήστης
+                    System.out.println("Δες τα στοιχεία του Υπουργείου που σε ενδιαφέρει:");
+                    MenuForMinistries menu = new MenuForMinistries(ministries);
+                    menu.Menu();
+                    break;
+                case 5: // Δυνατότητα εισαγωγής αλλαγών
+                    System.out.println("Δώσε τις επιθυμητές αλλαγές:");
+                    break;
+                case 6: // Γραφήματα
+                System.out.println("Δες τα γραφήματα:");
+                 try {
+            diagramministries.diagram();  
+                 } catch (Exception e) {
+            e.printStackTrace();
+            }
+                break;
+
+                case 100: // Έξοδος από την εφαρμογή
+                    System.out.println("Καληνύχτα κύριε πρωθυπουργέ!");
+                    break;
+                
+                default:
+                    System.out.println("Άκυρος αριθμός");
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static void Menu2025() {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = 0;
+        while (x != 100) {     
+            System.out.println("Δώσε αριθμό επιλογής για το μενού");
+            System.out.println("Για εμφάνιση στοιχείων όλων των υπουργείων πάτα 0");
+            System.out.println("Για εμφάνιση ποσοστών κάθε υπουργείου επί του συνολικού προϋπολογισμού πάτα 1");
+            System.out.println("Για εμφάνιση όλων των εσόδων πάτα 2");
+            System.out.println("Για εμφάνιση όλων των εξόδων πάτα 3");
+            System.out.println("Για εμφάνιση στοιχείων όλων των υπουργείων και επιλογή ενός πάτα 4");
+            System.out.println("Για εισαγωγή αλλαγών πάτα 5");
+            System.out.println("Για να εμφανιστούν γραφήματα πάτα 6:");
+            System.out.println("Για έξοδο από την εφαρμογή πάτα 100");
+            x = scanner.nextInt();
+
+            Ministries[] ministries = MinistryLoader.load("src/main/resources/config/ministries25.json");
+            Revenues[] revenues = RevenuesLoader.load("src/main/resources/config/revenues25.json");
+            Expenses[] expenses = ExpensesLoader.load("src/main/resources/config/expenses25.json");
+
+            switch (x) {
+                case 0: // Εμφάνιση στοιχείων όλων των υπουργείων
+                    MinistryPrinter ministryPrinter = new MinistryPrinter(ministries);
+                    ministryPrinter.display();
+                    break;
+
+                case 1: // Εμφάνιση ποσοστών κάθε υπουργείου επί του συνολικού προϋπολογισμού
+                    Ministrypercentage ministryPercentage = new Ministrypercentage(ministries);
+                    ministryPercentage.display();
+                    break;
+
+                case 2: // Εμφάνιση όλων των εσόδων 
+                    RevenuesPrinter revenuesPrinter = new RevenuesPrinter(revenues);
+                    revenuesPrinter.display();
+                    break;
+
+                case 3: // Εμφάνιση όλων των εξόδων
+                    ExpensesPrinter expensesPrinter = new ExpensesPrinter(expenses);
+                    expensesPrinter.display();
+                    break;
+
+                case 4: // Εμφάνιση προϋπολογισμού συγκεκριμένου υπουργείου που επιλέγει ο χρήστης
+                    System.out.println("Δες τα στοιχεία του Υπουργείου που σε ενδιαφέρει:");
+                    MenuForMinistries menu = new MenuForMinistries(ministries);
+                    menu.Menu();
+                    break;
+                case 5: // Δυνατότητα εισαγωγής αλλαγών
+                    System.out.println("Δώσε τις επιθυμητές αλλαγές:");
+                    break;
+                case 6: // Γραφήματα
+                System.out.println("Δες τα γραφήματα:");
+                 try {
+            diagramministries.diagram();  
+                 } catch (Exception e) {
+            e.printStackTrace();
+            }
+                break;
+
+                case 100: // Έξοδος από την εφαρμογή
+                    System.out.println("Καληνύχτα κύριε πρωθυπουργέ!");
+                    break;
+                
+                default:
+                    System.out.println("Άκυρος αριθμός");
+            }
+        }
+
+        scanner.close();
+    }
+}
