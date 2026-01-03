ebefe07ae251613b37f7848e9071877290579aea
Aristeidis Anagnostopoulos <t8230241@aueb.gr>
2026-01-03 16:42:53 +0200

    Created MainMenu, moved prints from Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,22 +4,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        
         Scanner scanner = new Scanner(System.in); // Ξεκινάει το μενού επιλογών του χρήστη
         int x = 0;
         System.out.println("ΕΠΙΛΟΓΗ ΠΡΟΙΠΟΛΟΓΙΣΜΟΥ ΕΤΟΥΣ¨:2023, 2024, 2025");
         int yearSelection = scanner.nextInt();
 
-            while (x != 100) {     
-                System.out.println("Δώσε αριθμό επιλογής για το μενού");
-                System.out.println("Για εμφάνιση στοιχείων όλων των υπουργείων πάτα 0");
-                System.out.println("Για εμφάνιση ποσοστών κάθε υπουργείου επί του συνολικού προϋπολογισμού πάτα 1");
-                System.out.println("Για εμφάνιση όλων των εσόδων πάτα 2");
-                System.out.println("Για εμφάνιση όλων των εξόδων πάτα 3");
-                System.out.println("Για εμφάνιση στοιχείων όλων των υπουργείων και επιλογή ενός πάτα 4");
-                System.out.println("Για εισαγωγή αλλαγών πάτα 5");
-                System.out.println("Για να εμφανιστούν γραφήματα πάτα 6:");
-                System.out.println("Για έξοδο από την εφαρμογή πάτα 100");
+            while (x != 100) {
+                MainMenu.mainMeNuOptionsPrinter();
                 x = scanner.nextInt();
 
                 String ministriesFile = "ministries" + yearSelection + ".json";
